a2d5ae21d25aa5439a90c6a10a4f5b5ffe6c5f84
PhongN18 <chelsea2009021808@gmail.com>
2025-01-06 23:50:37 +0700

    modify useEffect content

--- practice-hooks/src/Content.jsx
+++ practice-hooks/src/Content.jsx
@@ -5,12 +5,23 @@ function Content() {
 
     const [type, setType] = useState('posts')
     const [posts, setPosts] = useState([])
+    const [showToTop, setShowToTop] = useState(false)
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
             .then(posts => setPosts(posts))
     }, [type])
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            setShowToTop(window.scrollY >= 200)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+    }, [])
 
     return (
         <div>
@@ -31,6 +42,18 @@ function Content() {
                     <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
+
+            {showToTop && (
+                <button
+                    style={{
+                        position: 'fixed',
+                        right: 20,
+                        bottom: 20
+                    }}
+                >
+                    Go to top
+                </button>
+            )}
         </div>
     )
 }
